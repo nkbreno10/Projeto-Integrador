4097b97adc68d35a1049e7d112f64fd09388f544
nkbreno10 <50603874+nkbreno10@users.noreply.github.com>
2020-04-03 15:29:03 -0300

    Add files via upload

---
+++ cabecalho.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="author" content="URSA Soluções">
+  <meta name="title" content="URSA Soluções">
+  <meta name="description" content="Home Page URSA">
+  <meta name="keywords" content="criadora de softwares, gestão de empresas, soluções de negócios"> 
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/style.css">
+  <!--Title-->
+  <title img>URSA Soluções</title>
+  <link rel="URSA icon" href="images/ursa.jpeg">
+</head>
+<body>
+  <header>
+<!--<nav class="navbar navbar-light bg-light">
+  <a class="navbar-brand" href="#">
+    <img src="/docs/4.3/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
+    URSA Soluções
+  </a>
+</nav>-->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="indeaaax.php">URSA Soluções</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link" href="indeaaax.php">Sobre</a>
+      <a class="nav-item nav-link" href="indeaaax.php">Contato</a>
+      <a class="nav-login nav-link" favicon href="#" >Login</a>
+    </div>
+  </div>
+</nav>
+</header>
+<div class="container">
+                  <div class="row">
+                      <div class="col-lg-6 col-md-6 col-sm-12">
+                        <h5>Informações Adicionais</h5>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque interdum quam odio, quis placerat ante luctus eu. Sed aliquet dolor id sapien rutrum, id vulputate quam iaculis.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque interdum quam odio, quis placerat ante luctus eu. Sed aliquet dolor id sapien rutrum, id vulputate quam iaculis.</p>
+                      </div>
+                      <div class="col-lg-3 col-md-3 col-sm-12">
+                        <h4>Loja 1</h4>
+                        <p>Av. Paulista, 1000, São Paulo
+                          info@geek.com <br>
+                          + 55 11 99999-9999<br>
+                          + 55 11 88888-8888</p>
+                       </div>
+                      <div class="col-lg-3 col-md-3 col-sm-12">
+                        <h4>Loja 2</h4>
+                        <p> Av. Santo Amaro, 1000, São Paulo
+                            info@geek.com<br>
+                            + 55 11 77777-7777<br>
+                            + 55 11 66666-6666
+                        </p>     
+                      </div>
+                      </div>
+                      <p class="C">&copy;2019 Copyright: geek.com</p
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ contato.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8"/>
+    <title>URSA Soluções</title>
+    <link rel="URSA icon" href="images/ursa.jpeg">
+</head>
+<body>
+    <?php include_once 'cabecalho.php';?>
+</body>
+</html>
